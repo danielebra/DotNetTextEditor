895a511d241c54f92bfb8c6efca3a9b739f8482a
Daniel Ebrahimian <5339066+danielebra@users.noreply.github.com>
2018-10-19 11:48:53 +1100

    Add HotKey class

---
+++ DotNetAssignment2/DotNetAssignment2/Classes/HotKeys.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DotNetAssignment2.Classes
+{
+    // Define key combinations to perform actions
+    public static class HotKeys
+    {
+        public static bool Save(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.S);
+        }
+        public static bool Open(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.O);
+        }
+        public static bool New(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.N);
+        }
+        public static bool Cut(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.X);
+        }
+        public static bool Copy(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.C);
+        }
+        public static bool Paste(Keys keys)
+        {
+            return keys == (Keys.Control | Keys.V);
+        }
+    }
+}
